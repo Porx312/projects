be7adf6eee20efbd1e458ce42cb057d25685daa2
codeporx <codeporx@gmail.com>
2025-03-01 23:46:46 +0100

    first commit

---
+++ app/pokemon/page.tsx
@@ -0,0 +1,12 @@
+import Pokemon from '@/projects/PokeApi/Pokemon'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Pokemon/>
+    </div>
+  )
+}
+
+export default page
---
+++ projects/PokeApi/Pokemon.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Pokemon = () => {
+  return (
+    <div>
+      <h2>poke api</h2>
+    </div>
+  )
+}
+
+export default Pokemon
